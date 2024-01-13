20e299be4d9a2d1dfa72b6053e39cd58ccbd164a
ldandoy <ldandoy@overconsulting.net>
2024-01-13 15:53:05 +0100

    Ajout autoload

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,9 @@
 <?php
 
-    require_once('../classes/Twig.php');
+    require_once '../vendor/autoload.php';
 
+    use App\Twig;
+    
     $twig = new Twig();
 
     echo $twig->render('index.html.twig', ['name' => 'Fabien']);
