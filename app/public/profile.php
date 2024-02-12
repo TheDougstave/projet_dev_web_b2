882d32ffb488bbd4686e4a7eee7573d4ee90b6d1
TheDougstave <dylangesbert@outlook.fr>
2024-02-12 11:07:57 +0100

    ajout d'une vérif dans profile pour déconnecter si l'utilisateur n'est pas co

--- app/public/profile.php
+++ app/public/profile.php
@@ -8,6 +8,11 @@ $page = new Page();
 
 var_dump($page->session->get('user'));
 
+if(!$page->session->has('user')) {
+    header("Location: login.php");
+    exit();
+}
+
 echo $page->render('profil.html.twig',[
     'msg' => $msg
 ]);
