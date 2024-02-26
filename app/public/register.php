294cd44f81636e0ceeb9a93e9662d67e2d412dac
Linscoo <133121712+Linscoo@users.noreply.github.com>
2024-02-26 14:55:03 +0100

    changement dans le register

--- app/public/register.php
+++ app/public/register.php
@@ -1,21 +1,42 @@
 <?php
 
-    require_once '../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
-    use App\Page;
-    
-    $page = new Page();
+use App\Page;
 
-    if(isset($_POST['send'])){
-        //var_dump($_POST);
-   
-        
-        $page->insert('users', [
-            'email' => $_POST["email"],
-            'password' => password_hash($_POST["password"],PASSWORD_BCRYPT),
-            'role' => 0 //le role 0 c'est un user basique
-        ]);
+$page = new Page();
+$errors = []; // Initialiser le tableau des erreurs
+
+// Vérification formulaire soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Email invalide";
+    } elseif ($password !== $confirmPassword) {
+        $errors['confirmPassword'] = "Les mots de passe ne correspondent pas.";
+    } elseif (!preg_match('/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%*?&]{6,}$/', $password)) {
+        $errors['password'] = "Mot de passe incorrect";
+    } else {
+        if (!$page->checkEmailExists($email)) {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            try {
+                $page->insertUser($email, $hashedPassword);
+                // Redirection après une inscription réussie
+                header("Location: register.php?email=" . urlencode($email));
+                exit();
+            } catch (\Exception $e) {
+                $errors['database'] = $e->getMessage();
+            }
+        } else {
+            $errors['email'] = "L'adresse email est déjà utilisée.";
+        }
     }
+}
+
+
 
     echo $page->render('register.html.twig', []);
 
