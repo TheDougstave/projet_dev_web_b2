9ac3dc85e13051397ef753c031423a225b265206
Tay9875 <96668181+Tay9875@users.noreply.github.com>
2024-03-05 12:04:11 +0100

    Add files via upload

---
+++ b2-paris.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 05 mars 2024 à 11:03
+-- Version du serveur : 8.1.0
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `b2-paris`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `intervention`
+--
+
+DROP TABLE IF EXISTS `intervention`;
+CREATE TABLE IF NOT EXISTS `intervention` (
+  `IDI` int NOT NULL,
+  `DATE` date NOT NULL,
+  `CREATED_AT` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `DETAIL` varchar(1023) NOT NULL,
+  `NOM` varchar(50) NOT NULL,
+  `STATUT` int NOT NULL,
+  `ADRESSE` varchar(255) NOT NULL,
+  `URGENCE` int NOT NULL,
+  PRIMARY KEY (`IDI`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `intervient`
+--
+
+DROP TABLE IF EXISTS `intervient`;
+CREATE TABLE IF NOT EXISTS `intervient` (
+  `IDI` int NOT NULL,
+  `IDU` int NOT NULL,
+  KEY `INTERVENTION_INTERVIENT_FK` (`IDI`),
+  KEY `INTERVIENT_USER_FK` (`IDU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `IDU` int NOT NULL,
+  `EMAIL` varchar(50) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `ROLE` int NOT NULL,
+  `CREATED_AT` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `MODIFIED_AT` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`IDU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
